d581f3cdb0c3e43d0045da860320345cfb80788a
petrasvestartas <petrasvestartas@gmail.com>
2023-07-18 13:56:01 +0200

    MISC black formatter

--- src/compas_assembly2/assembly_datastructure/element.py
+++ src/compas_assembly2/assembly_datastructure/element.py
@@ -1,7 +1,7 @@
 from compas.geometry import Frame
 
 
-class Element():
+class Element:
     def __init__(self, id, l_frame, g_frame, attr=None):
         self.id = id  # tuple e.g. (0, 1) or (1, 5, 9)
         self.l_frame = l_frame if l_frame is not None else Frame.worldXY()
