fb14888eeb5b262dc274fe2ff6bc2ea1cd92ab52
petrasvestartas <petrasvestartas@gmail.com>
2023-07-18 13:17:01 +0200

    MISC build test

--- src/compas_assembly2/assembly_datastructure/element.py
+++ src/compas_assembly2/assembly_datastructure/element.py
@@ -6,17 +6,9 @@ from compas.geometry import Frame
 
 
 class Element():
+    """A data structure for the individual elements of a discrete element assembly."""
 
     def __init__(self, id, l_frame, g_frame=None, attr=None):
-        """A data structure for the individual elements of a discrete element assembly."""
-    #     """
-    #     Initialize an Element with a value, an id tuple, and a dictionary of keys.
-
-    #     Parameters:
-    #         id (tuple): The id tuple associated with the Element for ordering.
-    #         frame (compas.geometry.Frame): The frame associated with the Element.
-    #         attr (dict): A dictionary of keys associated with the Element.
-    #     """
         self.id = id  # tuple e.g. (0, 1) or (1, 5, 9)
         self.l_frame = l_frame if l_frame is not None else Frame.worldXY()
         self.g_frame = g_frame if g_frame is not None else Frame.worldXY()
