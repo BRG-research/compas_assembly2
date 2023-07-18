d0dcab76b90bf48b450a3b9a3dd0140ecb6807e1
petrasvestartas <petrasvestartas@gmail.com>
2023-07-18 14:07:22 +0200

    MISC test black formatter

--- src/compas_assembly2/assembly_datastructure/element.py
+++ src/compas_assembly2/assembly_datastructure/element.py
@@ -1,11 +1,16 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
 from compas.geometry import Frame
 
 
 class Element:
+    """A data structure for the individual elements of an assembly."""
+
     def __init__(self, id, l_frame, g_frame, attr=None):
         self.id = id  # tuple e.g. (0, 1) or (1, 5, 9)
-        self.l_frame = l_frame if l_frame is not None else Frame.worldXY()
-        self.g_frame = g_frame if g_frame is not None else Frame.worldXY()
+        self.l_frame = l_frame or Frame.worldXY()
+        self.g_frame = g_frame or Frame.worldXY()
         self.fabrication = dict()
         self.structure = dict()
         self.attr = attr  # dict e.g. {"type": Block, "mass": 20}
