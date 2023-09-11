f98026e1f5c3b626b59b2a20a9b350569d5993db
Petras Vestartas <petrasvestartas@gmail.com>
2023-09-11 16:55:33 +0200

    ADD add_by_index and merge_assemblies in a tree structure

--- src/tree_data_structure_merge_v2.py
+++ src/tree_data_structure_merge_v2.py
@@ -1,17 +1,29 @@
-class Assembly_Node:
-    def __init__(self, name, data, childs=None):
-        self.name = name if name else "assembly_root"
-        self.data = data  # data is empty if it is assembly and not an element
-        self.children = childs if childs is not None else []
+import uuid
+
+# Path - Breanch, Element - Node Lead
+
+class Assembly_Tree:
+    def __init__(self, branch, twigs=None):
+        self.branch = branch  # branch is empty if it is branch and not an element
+        self.twigs = twigs if twigs is not None else []  # better have a sorted_dict <__str__, Assembly_Tree> currently only list(Assembly_Tree)
         self.parent = None
+        self.is_leaf = False
 
     def add_child(self, child):
         child.parent = self
-        self.children.append(child)
+        self.twigs.append(child)
 
     @property
-    def full_name(self):
-        return self.name + "_" + str(self.data)
+    def path(self):
+        """The branch must have a meaning full string representation e.g. path_address
+        path can be an index, an element type or any other string representation
+        address is a memory value to identify duplicates
+        """
+        return str(self.branch)
+
+    @property
+    def path_str(self):
+        return str(self.path)
 
     @property
     def level(self):
@@ -33,121 +45,118 @@ class Assembly_Node:
             depth += 1
             current_node = current_node.parent
 
-        # Calculate the maximum depth by traversing children's subtrees
+        # Calculate the maximum depth by traversing twigs's subtrees
         return self._calculate_depth(self, depth)
 
     def _calculate_depth(self, node, depth):
-        if not node.children:
-            # If the node has no children, return the current depth
+        if not node.twigs:
+            # If the node has no twigs, return the current depth
             return depth
 
         max_child_depth = depth
-        for child in node.children:
+        for child in node.twigs:
             # Recursively calculate the depth for each child's subtree
             child_depth = self._calculate_depth(child, depth + 1)
             if child_depth > max_child_depth:
                 max_child_depth = child_depth
 
-        # Return the maximum depth among children's subtrees
+        # Return the maximum depth among twigs's subtrees
         return max_child_depth
 
     def print_tree(self):
-        prefix = "  " * self.level
+        prefix = "   " * self.level
         prefix = prefix + "|__ " if self.parent else ""
-        print(prefix + self.name + " " + self.data)
-        if self.children:
-            for child in self.children:
+        print(prefix + self.path_str)
+        if self.twigs:
+            for child in self.twigs:
                 child.print_tree()
 
-    def truncate_tree(self, value_to_remove):
-        # Create a list to store nodes to remove
-        nodes_to_remove = []
+    def merge_assemblies(self, a1, allow_duplicates=True):
 
-        # Traverse the tree to identify nodes with the given value
-        def find_nodes_to_remove(node):
-            if node.data == value_to_remove:
-                nodes_to_remove.append(node)
-
-            for child in node.children:
-                find_nodes_to_remove(child)
-
-        find_nodes_to_remove(self)
-
-        # Remove the identified nodes and update parent-child relationships
-        for node in nodes_to_remove:
-            if node.parent:
-                node.parent.children.remove(node)
-                node.parent = None
-
-    def merge_assemblies(self, _a1, allow_duplicate_data=True):
-        # Helper function to find a node with the same name in a list of nodes
-        def find_node_by_full_name(nodes, node_a1):
-            if allow_duplicate_data:
+        # Helper function to find a node with the same path in a list of nodes
+        def find_node_by_path(nodes, node_a1):
+            if allow_duplicates or a1.is_leaf or node_a1.is_leaf:
                 return None
             else:
                 for node in nodes:
-                    if allow_duplicate_data:
-                        if node.name == node_a1.name:
-                            return node
-                    else:
-                        if node.full_name == node_a1.full_name:
-                            return node
+                    if node.path == node_a1.path:
+                        return node
                 return None
 
-        # for the very first one wrap it the node to another node
-        #a1 = Assembly_Node("WARNING_TEMP_ASSEMBLY", _a1) if (_a1.level == 0) else _a1
-        a1 = _a1
-        print(a1.level)
-
         # Iterate through the nodes in a1
-        for node_a1 in a1.children:
+        for node_a1 in a1.twigs:
             # Check if there is an equivalent node in a0
-            existing_node_a0 = find_node_by_full_name(self.children, node_a1)
-            if existing_node_a0:
-                # Recursively merge the children of the two nodes
-                existing_node_a0.merge_assemblies(node_a1, allow_duplicate_data)
+            print(node_a1.is_leaf)
+            existing_node_a0 = find_node_by_path(self.twigs, node_a1)
+            if existing_node_a0 is not None:
+                # Recursively merge the twigs of the two nodes
+                existing_node_a0.merge_assemblies(node_a1, allow_duplicates)
             else:
                 # If no corresponding node is found, add the node from a1 to a0
                 self.add_child(node_a1)
 
+    def add_by_index(self, name_list, data):
+        # create branch
 
-def add_by_index(node):
-    # if the name is given as a level add the child to the assembly
-    pass
+        branch_tree = Assembly_Tree("temp")
+        last_branch = branch_tree
+        for name in name_list:
+            child = Assembly_Tree(name)
+            last_branch.add_child(child)
+            last_branch = child
+
+        # add "real" data to the last branch
+        data_node = Assembly_Tree(data)
+        data_node.is_leaf = True
+        last_branch.add_child(data_node)
+
+        # merge this branch with the rest
+        self.merge_assemblies(branch_tree, False)
 
 
 def build_product_tree_0():
-    # a0 assembly
-    root = Assembly_Node("1", "Oktoberfest")
-    laptop = Assembly_Node("2", "Schnitzel_Haus")
-    keyboard = Assembly_Node("3", "Bier")
-    screen = Assembly_Node("3", "Schnitzel")
-    laptop.add_child(keyboard)
-    laptop.add_child(screen)
-    root.add_child(laptop)
+    # a0 branch
+    root = Assembly_Tree("Oktoberfest")
+    Schnitzel_Haus = Assembly_Tree("SchnitzelHaus")
+    Bier = Assembly_Tree("Bier")
+    Schnitzel = Assembly_Tree("Schnitzel")
+    Sauce = Assembly_Tree("Sauce")
+    Schnitzel.add_child(Sauce)
+    Schnitzel_Haus.add_child(Bier)
+    Schnitzel_Haus.add_child(Schnitzel)
+    root.add_child(Schnitzel_Haus)
+
     # print(root.depth)
-    # print(root.children[0].depth)
+    # print(root.twigs[0].depth)
     return root
 
 
 def build_product_tree_1():
-    # a1 assembly
-    root = Assembly_Node("1", "Oktoberfest")
-    laptop = Assembly_Node("2", "Schnitzel_Haus")
-    keyboard = Assembly_Node("3", "Schnitzel")
-    screen = Assembly_Node("3", "Wurstsalat")
-    laptop.add_child(keyboard)
-    laptop.add_child(screen)
-    root.add_child(laptop)
+    # a1 branch
+    root = Assembly_Tree("Oktoberfest")
+    Schnitzel_Haus = Assembly_Tree("SchnitzelHaus")
+    Bier = Assembly_Tree("Bier")
+    Bier2 = Assembly_Tree("Bier2")
+    Schnitzel_Haus.add_child(Bier2)
+    Schnitzel = Assembly_Tree("Schnitzel")
+    Sauce = Assembly_Tree("Sauce")
+    Sauce_White = Assembly_Tree("Sauce_White")
+    Schnitzel.add_child(Sauce)
+    Schnitzel.add_child(Sauce_White)
+    Schnitzel_Haus.add_child(Bier)
+    Schnitzel_Haus.add_child(Schnitzel)
+    root.add_child(Schnitzel_Haus)
     # print(root.depth)
-    # print(root.children[0].depth)
+    # print(root.twigs[0].depth)
     return root
 
 
 if __name__ == "__main__":
     a0 = build_product_tree_0()
-    # a0.print_tree()
-    a1 = build_product_tree_1()
-    a0.merge_assemblies(Assembly_Node("-1", "WARNING_TEMP_ASSEMBLY", [a1]), True)
     a0.print_tree()
-    pass
+    a1 = build_product_tree_1()
+    a1.print_tree()
+    a0.merge_assemblies(a1, True)
+    a0.add_by_index(["SchnitzelHaus", "Schnitzel", "Sauce_White"], "_____Cream____")
+    a0.add_by_index(["SchnitzelHaus", "Schnitzel", "Sauce_White"], "_____Cream____")
+    a0.print_tree()
