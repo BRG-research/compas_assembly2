9927525882bd874bc5c944d58b2bceda6f0b8918
petrasvestartas <petrasvestartas@gmail.com>
2023-10-02 19:51:54 +0200

    MISC move collision example to docstring

--- src/compas_assembly2/assembly.py
+++ src/compas_assembly2/assembly.py
@@ -1552,11 +1552,11 @@ class Assembly(Data):
         >>> from compas_assembly2 import ELEMENT_NAME
         >>> #
         >>> b1 = Element(
-        >>>     name=ELEMENT_NAME.BLOCK,
-        >>>     id=0,
-        >>>     frame=Frame.worldXY,
-        >>>     simplex=Point(0, 0, 0),
-        >>>     complex=Box.from_width_height_depth(0.5, 0.5, 0.5))
+        ...     name=ELEMENT_NAME.BLOCK,
+        ...     id=0,
+        ...     frame=Frame.worldXY,
+        ...     simplex=Point(0, 0, 0),
+        ...     complex=Box.from_width_height_depth(0.5, 0.5, 0.5))
         >>> #
         >>> num_copies = 200
         >>> max_translation = 8  # Maximum translation distance from the center
