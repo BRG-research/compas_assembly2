9f61014ca6c047ea67fe05a9b627a3e9abaadcf0
petrasvestartas <petrasvestartas@gmail.com>
2023-10-09 15:19:13 +0200

    ADD model_tree data structure basis

--- src/model.py
+++ src/model.py
@@ -26,80 +26,191 @@ class ModelNode(TreeNode):
         else:
             return False
 
+    def __getitem__(self, index):
+        # --------------------------------------------------------------------------
+        # sanity checks
+        # --------------------------------------------------------------------------
+        if not isinstance(index, int):
+            raise TypeError("The index is not integer.")
+
+        # --------------------------------------------------------------------------
+        # return the child
+        # --------------------------------------------------------------------------
+        return self._children[index]
+
+    def __setitem__(self, index, model_node):
+        # --------------------------------------------------------------------------
+        # sanity checks
+        # --------------------------------------------------------------------------
+        if not isinstance(model_node, ModelNode):
+            raise TypeError("The node is not a ModelNode object.")
+
+        # --------------------------------------------------------------------------
+        # change the node of the tree and update the node's tree and parent
+        # --------------------------------------------------------------------------
+        self._children[index] = model_node
+        self._children[index]._tree = self._tree
+        self._children[index]._parent = self
+
+    def add(self, node):
+        """
+        Add a child node to this node.
+        WARNING: this function is overridden to use SortedList instead of set().
+
+        Returns
+        -------
+        None
+
+        """
+        if not isinstance(node, TreeNode):
+            raise TypeError("The node is not a TreeNode object.")
+        if node not in self._children:
+            self._children.add(node)
+        node._parent = self
 
 class Model(Tree):
     def __init__(self, name="", attributes=None):
-        super(Tree, self).__init__()
+        super(Model, self).__init__(name=name, attributes=attributes)
+        self.name = name  # Set the 'name' as an instance variable
         self._elements = OrderedDict()  # a flat collection of elements - dict{GUID, Element}
-        self._nodes = SortedList()  # hierarchical relationships between elements
+        self._root = None  # hierarchical relationships between elements
         self._interactions = Graph()  # abstract linkage or connection between elements
-
         # --------------------------------------------------------------------------
-        # simplifying the root node creation
+        # create root directly in the constructor
         # --------------------------------------------------------------------------
-
-        # add_root method is called so that the user would not need to call add root each time
         if name != "":
-            message = (
-                "\n---------------------------------------------------------------------------------------------------"
-            )
-            message += "\n NOTE: the root node is added to the Model by the first user input: <name>"
-            message += (
-                "\n       if you do not want to specify the name, create a ModelNode, then run .add_root() method"
-            )
-            message += (
-                "\n---------------------------------------------------------------------------------------------------"
-            )
-            print(message)
-            self.add_root(ModelNode(name=name))
-            self.name = "model"
+            self.add(ModelNode(name=name))
+
+    def __getitem__(self, index):
+        # --------------------------------------------------------------------------
+        # sanity checks
+        # --------------------------------------------------------------------------
+        if not isinstance(index, int):
+            raise TypeError("The index is not integer.")
+
+        # --------------------------------------------------------------------------
+        # return the node
+        # --------------------------------------------------------------------------
+        print("__________get__item_______")
+        return self._root._children[index]
+
+    def __setitem__(self, index, model_node):
+
+        # --------------------------------------------------------------------------
+        # sanity checks
+        # --------------------------------------------------------------------------
+        if not isinstance(model_node, ModelNode):
+            raise TypeError("The node is not a ModelNode object.")
+
+        # --------------------------------------------------------------------------
+        # change the node of the tree and update the node's tree and parent
+        # --------------------------------------------------------------------------
+        self._root._children.add(model_node)
+        self._nodes[index]._tree = self
+        self._nodes[index]._parent = self
+
+        # --------------------------------------------------------------------------
+        # if index is 0, then set all properties related to the root node
+        # --------------------------------------------------------------------------
+        if index == 0:
+            self._root._children = self._nodes[index]
+
+    def add(self, node, parent=None):
+        """
+        Add a node to the tree.
+
+        Parameters
+        ----------
+        node : :class:`~compas.datastructures.TreeNode`
+            The node to add.
+        parent : :class:`~compas.datastructures.TreeNode`, optional
+            The parent node of the node to add.
+            Default is ``None``, in which case the node is added as a root node.
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        TypeError
+            If the node is not a :class:`~compas.datastructures.TreeNode` object.
+            If the supplied parent node is not a :class:`~compas.datastructures.TreeNode` object.
+        ValueError
+            If the node is already part of another tree.
+            If the supplied parent node is not part of this tree.
+            If the tree already has a root node, when trying to add a root node.
+
+        """
+        if not isinstance(node, TreeNode):
+            raise TypeError("The node is not a TreeNode object.")
+
+        if node.parent:
+            raise ValueError("The node already has a parent, remove it from that parent first.")
+
+        if parent is None:
+            # add the node as a root node
+            if self.root is not None:
+                self._root.add(node)
+                node._tree = self._root
+                return
+                raise ValueError("The tree already has a root node, remove it first.")
+
+            self._root = node
+            node._tree = self
+
         else:
-            message = (
-                "\n---------------------------------------------------------------------------------------------------"
-            )
-            message += "\nWARNING: Create root by first defining a ModelNode and then calling the .add_root() method"
-            message += (
-                "\n---------------------------------------------------------------------------------------------------"
-            )
+            # add the node as a child of the parent node
+            if not isinstance(parent, TreeNode):
+                raise TypeError("The parent node is not a TreeNode object.")
 
+            if parent.tree is not self:
+                raise ValueError("The parent node is not part of this tree.")
 
-def create_tree_with_root():
-    tree = Model("my_model_tree")
-    # root = ModelNode("root")
-    # tree.add_root(root)
-    # print
-    print(tree)
+            parent.add(node)
+
+def create_tree():
+    tree = Tree()
+    root = TreeNode("root")
+    branch = TreeNode("branch")
+    leaf1 = TreeNode("leaf1")
+    leaf2 = TreeNode("leaf2")
+    tree.add(root)
+    root.add(branch)
+    branch.add(leaf1)
+    branch.add(leaf2)
+    print(tree, tree.__class__.__name__)
+    print("_" * 100)
     tree.print()
-    return tree
 
 
-def create_tree_with_root_and_branches():
-    tree = Model("my_model_tree")
-    branch = ModelNode("branch")
-    tree.root.add(branch)
-    # print
-    print(tree)
-    tree.print()
-    return tree
-
-
-def create_tree_with_root_and_branches_and_leaves():
-    tree = Model("my_model_tree")
+def create_model_tree():
+    tree = Model()
+    root = ModelNode("root")
     branch = ModelNode("branch")
     leaf1 = ModelNode("leaf1")
     leaf2 = ModelNode("leaf2")
-    # tree.add_root(root)
-    tree.root.add(branch)
+    tree.add(root)
+    root.add(branch)
     branch.add(leaf1)
     branch.add(leaf2)
-    # print
-    print(tree)
+    print(tree, tree.__class__.__name__)
+    print("_" * 100)
     tree.print()
-    return tree
+
+
+def create_model_tree_operators():
+    tree = Model("root")
+    tree.add(ModelNode("structure"))
+    tree[0].add(ModelNode("timber"))
+    tree[0].add(ModelNode("concrete"))
+    print(tree, tree.__class__.__name__)
+    print("_" * 100)
+    tree.print()
 
 
 if __name__ == "__main__":
-    # create_tree_with_root()
-    # create_tree_with_root_and_branches()
-    create_tree_with_root_and_branches_and_leaves()
+    # tree = create_tree()
+    model_tree = create_model_tree_operators()
+    # tree = create_tree_via_getter()
     pass
--- src/tree.py
+++ src/tree.py
@@ -3,48 +3,60 @@ from __future__ import absolute_import
 from __future__ import division
 
 from compas.datastructures import Datastructure
+from compas.data import Data
 
 
-class TreeNode(object):
+class TreeNode(Data):
     """A node of a tree data structure.
 
     Parameters
     ----------
-    name: str, optional
+    name : str, optional
         The name of the tree ndoe.
-    attributes: dict[str, Any], optional
+    attributes : dict[str, Any], optional
         User-defined attributes of the datastructure.
 
     Attributes
     ----------
-    name: str
+    name : str
         The name of the datastructure.
     attributes : dict[str, Any]
         User-defined attributes of the datastructure.
-    parent: :class:`~compas.datastructures.TreeNode`
+    parent : :class:`~compas.datastructures.TreeNode`
         The parent node of the tree node.
-    children: set[:class:`~compas.datastructures.TreeNode`]
+    children : list[:class:`~compas.datastructures.TreeNode`]
         The children of the tree node.
-    tree: :class:`~compas.datastructures.Tree`
-        The tree the node belongs to.
-    is_root: bool
+    tree : :class:`~compas.datastructures.Tree`
+        The tree to which the node belongs.
+    is_root : bool
         True if the node is the root node of the tree.
-    is_leaf: bool
+    is_leaf : bool
         True if the node is a leaf node of the tree.
-    is_branch: bool
+    is_branch : bool
         True if the node is a branch node of the tree.
-    acestors: generator[:class:`~compas.datastructures.TreeNode`]
+    acestors : generator[:class:`~compas.datastructures.TreeNode`]
         A generator of the acestors of the tree node.
-    descendants: generator[:class:`~compas.datastructures.TreeNode`]
-        A generator of the descendants of the tree node.
+    descendants : generator[:class:`~compas.datastructures.TreeNode`]
+        A generator of the descendants of the tree node, using a depth-first preorder traversal.
 
     """
 
+    DATASCHEMA = {
+        "type": "object",
+        "$recursiveAnchor": True,
+        "properties": {
+            "name": {"type": "string"},
+            "attributes": {"type": "object"},
+            "children": {"type": "array", "items": {"$recursiveRef": "#"}},
+        },
+        "required": ["name", "attributes", "children"],
+    }
+
     def __init__(self, name=None, attributes=None):
-        self.name = name
+        super(TreeNode, self).__init__(name=name)
         self.attributes = attributes or {}
         self._parent = None
-        self._children = set()
+        self._children = []
         self._tree = None
 
     def __repr__(self):
@@ -72,25 +84,67 @@ class TreeNode(object):
 
     @property
     def tree(self):
-        return self._tree
+        if self.is_root:
+            return self._tree
+        else:
+            return self.parent.tree
+
+    @property
+    def data(self):
+        return {
+            "name": self.name,
+            "attributes": self.attributes,
+            "children": [child.data for child in self.children],
+        }
+
+    @classmethod
+    def from_data(cls, data):
+        node = cls(data["name"], data["attributes"])
+        for child in data["children"]:
+            node.add(cls.from_data(child))
+        return node
 
     def add(self, node):
-        """Add a child node to this node."""
+        """
+        Add a child node to this node.
+
+        Parameters
+        ----------
+        node : :class:`~compas.datastructures.TreeNode`
+            The node to add.
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        TypeError
+            If the node is not a :class:`~compas.datastructures.TreeNode` object.
+
+        """
         if not isinstance(node, TreeNode):
             raise TypeError("The node is not a TreeNode object.")
-        self._children.add(node)
+        if node not in self._children:
+            self._children.append(node)
         node._parent = self
-        node._tree = self.tree
-        if self.tree:
-            self.tree.nodes.add(node)
 
     def remove(self, node):
-        """Remove a child node from this node."""
+        """
+        Remove a child node from this node.
+
+        Parameters
+        ----------
+        node : :class:`~compas.datastructures.TreeNode`
+            The node to remove.
+
+        Returns
+        -------
+        None
+
+        """
         self._children.remove(node)
         node._parent = None
-        node._tree = None
-        if self.tree:
-            self.tree.nodes.remove(node)
 
     @property
     def ancestors(self):
@@ -106,43 +160,76 @@ class TreeNode(object):
             for descendant in child.descendants:
                 yield descendant
 
-    def traverse(self, strategy="preorder"):
-        """Traverse the tree from this node."""
-        if strategy == "preorder":
-            yield self
-            for child in self.children:
-                for node in child.traverse(strategy):
-                    yield node
-        elif strategy == "postorder":
-            for child in self.children:
-                for node in child.traverse(strategy):
-                    yield node
-            yield self
+    def traverse(self, strategy="depthfirst", order="preorder"):
+        """
+        Traverse the tree from this node.
+
+        Parameters
+        ----------
+        strategy : {"depthfirst", "breadthfirst"}, optional
+            The traversal strategy.
+            Default is ``"depthfirst"``.
+
+        order : {"preorder", "postorder"}, optional
+            The traversal order. This parameter is only used for depth-first traversal.
+            Default is ``"preorder"``.
+
+        Yields
+        ------
+        :class:`~compas.datastructures.TreeNode`
+            The next node in the traversal.
+
+        Raises
+        ------
+        ValueError
+            If the strategy is not ``"depthfirst"`` or ``"breadthfirst"``.
+            If the order is not ``"preorder"`` or ``"postorder"``.
+
+        """
+        if strategy == "depthfirst":
+            if order == "preorder":
+                yield self
+                for child in self.children:
+                    for node in child.traverse(strategy, order):
+                        yield node
+            elif order == "postorder":
+                for child in self.children:
+                    for node in child.traverse(strategy, order):
+                        yield node
+                yield self
+            else:
+                raise ValueError("Unknown traversal order: {}".format(order))
+        elif strategy == "breadthfirst":
+            queue = [self]
+            while queue:
+                node = queue.pop(0)
+                yield node
+                queue.extend(node.children)
         else:
             raise ValueError("Unknown traversal strategy: {}".format(strategy))
 
 
 class Tree(Datastructure):
-    """A tree data structure.
+    """A hierarchical data structure that organizes elements into parent-child relationships. The tree starts from a unique root node, and every node (excluding the root) has exactly one parent.
 
     Parameters
     ----------
-    name: str, optional
+    name : str, optional
         The name of the datastructure.
-    attributes: dict[str, Any], optional
+    attributes : dict[str, Any], optional
         User-defined attributes of the datastructure.
 
     Attributes
     ----------
-    name: str
+    name : str
         The name of the datastructure.
     attributes : dict[str, Any]
         User-defined attributes of the datastructure.
-    root: :class:`~compas.datastructures.TreeNode`
+    root : :class:`~compas.datastructures.TreeNode`
         The root node of the tree.
-    nodes: set[:class:`~compas.datastructures.TreeNode`]
+    nodes : generator[:class:`~compas.datastructures.TreeNode`]
         The nodes of the tree.
-    leaves: generator[:class:`~compas.datastructures.TreeNode`]
+    leaves : generator[:class:`~compas.datastructures.TreeNode`]
         A generator of the leaves of the tree.
 
     Examples
@@ -153,7 +240,7 @@ class Tree(Datastructure):
     >>> branch = TreeNode('branch')
     >>> leaf1 = TreeNode('leaf1')
     >>> leaf2 = TreeNode('leaf2')
-    >>> tree.add_root(root)
+    >>> tree.add(root)
     >>> root.add(branch)
     >>> branch.add(leaf1)
     >>> branch.add(leaf2)
@@ -167,80 +254,114 @@ class Tree(Datastructure):
 
     """
 
-    JSONSCHEMA = {}
+    DATASCHEMA = {
+        "type": "object",
+        "properties": {
+            "name": {"type": "string"},
+            "root": TreeNode.DATASCHEMA,
+            "attributes": {"type": "object"},
+        },
+        "required": ["name", "root", "attributes"],
+    }
 
     def __init__(self, name=None, attributes=None):
-        super(Tree, self).__init__()
-        self.name = name
-        self.attributes = attributes or {}
+        super(Tree, self).__init__(name=name)
+        self.attributes.update(attributes or {})
         self._root = None
-        self._nodes = set()
 
     @property
     def data(self):
-        def get_node_data(node):
-            return {
-                "name": node.name,
-                "attributes": node.attributes,
-                "children": [get_node_data(child) for child in node.children],
-            }
-
         return {
             "name": self.name,
-            "root": get_node_data(self.root),
+            "root": self.root.data,
             "attributes": self.attributes,
         }
 
-    @data.setter
-    def data(self, data):
-        self.name = data["name"]
-        self.attributes = data["attributes"]
-
-        def node_from_data(data):
-            node = TreeNode(data["name"], data["attributes"])
-            for child in data["children"]:
-                node.add(node_from_data(child))
-            return node
-
-        self.add_root(node_from_data(data["root"]))
+    @classmethod
+    def from_data(cls, data):
+        tree = cls(data["name"], data["attributes"])
+        root = TreeNode.from_data(data["root"])
+        tree.add(root)
+        return tree
 
     @property
     def root(self):
         return self._root
 
-    def add_root(self, node):
-        """Add a root node to the tree."""
-        if not isinstance(node, TreeNode):
-            raise TypeError("The node is not a TreeNode object.")
-        if not node.is_root:
-            raise ValueError("The node is already part of another tree.")
-        self._root = node
-        node._tree = self
-        self._nodes.add(node)
+    def add(self, node, parent=None):
+        """
+        Add a node to the tree.
 
-    def add(self, node, parent):
-        """Add a node to the tree."""
+        Parameters
+        ----------
+        node : :class:`~compas.datastructures.TreeNode`
+            The node to add.
+        parent : :class:`~compas.datastructures.TreeNode`, optional
+            The parent node of the node to add.
+            Default is ``None``, in which case the node is added as a root node.
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        TypeError
+            If the node is not a :class:`~compas.datastructures.TreeNode` object.
+            If the supplied parent node is not a :class:`~compas.datastructures.TreeNode` object.
+        ValueError
+            If the node is already part of another tree.
+            If the supplied parent node is not part of this tree.
+            If the tree already has a root node, when trying to add a root node.
+
+        """
         if not isinstance(node, TreeNode):
             raise TypeError("The node is not a TreeNode object.")
-        if self.root is None:
-            raise ValueError("The tree has no root node, use add_root() first.")
+
+        if node.parent:
+            raise ValueError("The node already has a parent, remove it from that parent first.")
+
+        if parent is None:
+            # add the node as a root node
+            if self.root is not None:
+                raise ValueError("The tree already has a root node, remove it first.")
+
+            self._root = node
+            node._tree = self
+
         else:
+            # add the node as a child of the parent node
+            if not isinstance(parent, TreeNode):
+                raise TypeError("The parent node is not a TreeNode object.")
+
+            if parent.tree is not self:
+                raise ValueError("The parent node is not part of this tree.")
+
             parent.add(node)
 
     @property
     def nodes(self):
-        return self._nodes
-
-    def _remove_root(self):
-        """Remove the root node from the tree."""
-        self._root._tree = None
-        self.nodes.remove(self._root)
-        self._root = None
+        if self.root:
+            for node in self.root.traverse():
+                yield node
 
     def remove(self, node):
-        """Remove a node from the tree."""
+        """
+        Remove a node from the tree.
+
+        Parameters
+        ----------
+        node : :class:`~compas.datastructures.TreeNode`
+            The node to remove.
+
+        Returns
+        -------
+        None
+
+        """
         if node == self.root:
-            self._remove_root()
+            self._root = None
+            node._tree = None
         else:
             node.parent.remove(node)
 
@@ -250,8 +371,78 @@ class Tree(Datastructure):
             if node.is_leaf:
                 yield node
 
+    def traverse(self, strategy="depthfirst", order="preorder"):
+        """
+        Traverse the tree from the root node.
+
+        Parameters
+        ----------
+        strategy : {"depthfirst", "breadthfirst"}, optional
+            The traversal strategy.
+            Default is ``"depthfirst"``.
+
+        order : {"preorder", "postorder"}, optional
+            The traversal order. This parameter is only used for depth-first traversal.
+            Default is ``"preorder"``.
+
+        Yields
+        ------
+        :class:`~compas.datastructures.TreeNode`
+            The next node in the traversal.
+
+        Raises
+        ------
+        ValueError
+            If the strategy is not ``"depthfirst"`` or ``"breadthfirst"``.
+            If the order is not ``"preorder"`` or ``"postorder"``.
+
+        """
+        if self.root:
+            for node in self.root.traverse(strategy=strategy, order=order):
+                yield node
+
+    def get_node_by_name(self, name):
+        """
+        Get a node by its name.
+
+        Parameters
+        ----------
+        name : str
+            The name of the node.
+
+        Returns
+        -------
+        :class:`~compas.datastructures.TreeNode`
+            The node.
+
+        """
+        for node in self.nodes:
+            if node.name == name:
+                return node
+
+    def get_nodes_by_name(self, name):
+        """
+        Get all nodes by their name.
+
+        Parameters
+        ----------
+        name : str
+            The name of the node.
+
+        Returns
+        -------
+        list[:class:`~compas.datastructures.TreeNode`]
+            The nodes.
+
+        """
+        nodes = []
+        for node in self.nodes:
+            if node.name == name:
+                nodes.append(node)
+        return nodes
+
     def __repr__(self):
-        return "<Tree with {} nodes>".format(len(self.nodes))
+        return "<Tree with {} nodes>".format(len(list(self.nodes)))
 
     def print(self):
         """Print the spatial hierarchy of the tree."""
