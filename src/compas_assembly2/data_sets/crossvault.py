357134ee89a46e771ad065899499ff70c6eb6410
petrasvestartas <petrasvestartas@gmail.com>
2023-11-14 18:27:26 +0100

    ADD armadillo vault and cross vault examples re-serialized from v1 to v2

--- src/compas_assembly2/data_sets/crossvault.py
+++ src/compas_assembly2/data_sets/crossvault.py
@@ -1,12 +1,29 @@
 from compas.data import json_load
-
+from compas.geometry import Frame
+from compas_assembly2 import Element, Model, ModelNode, ViewerModel
 
 if __name__ == "__main__":
 
-    meshes = json_load("src/compas_assembly2/data_sets/crossvault.json")
+    meshes = json_load("src/compas_assembly2/data_sets/cross_vault_compas2.json")
 
-    # arch = Arch(rise=1, span=20, thickness=0.5, depth=10, n=5)
-    # model = arch.blocks()
-    # print(model)
-    # model.print()
-    # ViewerModel.run(model, scale_factor=1)
+    # convert meshes to elements and the simplex is the mesh center
+    elements = []
+    for mesh in meshes:
+        mid_point = mesh.centroid()
+        x_axis = mesh.face_normal(0)
+        y_axis = mesh.face_normal(1)
+        frame = Frame(point=mid_point, xaxis=x_axis, yaxis=y_axis)
+        element = Element(simplex=mid_point, complex=[mesh], frame=frame)
+        elements.append(element)
+
+    # --------------------------------------------------------------------------
+    # create model
+    # --------------------------------------------------------------------------
+    model = Model(name="cross_vault")
+    model.hierarchy.add(ModelNode("blocks", elements=elements))
+    model.print()
+
+    # --------------------------------------------------------------------------
+    # Vizualize it
+    # --------------------------------------------------------------------------
+    ViewerModel.run(model, scale_factor=1)
