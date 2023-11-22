05343e904d7912dec8894653b8e454d73d1b2b7f
Petras Vestartas <petrasvestartas@gmail.com>
2023-11-22 10:15:39 +0100

    SWICH to linux

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,11 +1,7 @@
-from compas_assembly2 import Element
-from compas.geometry import Point
+from compas.datastructures import Mesh
 
-element = Element(name="beam", geometry_simplified=Point(0, 0, 0))
-element_guid = element.guid
-print(element_guid)
-element = Element(name="beam", geometry_simplified=Point(0, 0, 0))
-element_guid2 = element.guid
-print(element_guid2)
-
-print(element_guid, element_guid2)
+mesh = Mesh.from_polyhedron(6)
+V = mesh.number_of_vertices()
+E = mesh.number_of_edges()
+F = mesh.number_of_faces()
+mesh.euler() == V - E + F
