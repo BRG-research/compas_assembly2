907cca063c387274908c6e0f87cf3398a5dd2503
Petras Vestartas <petrasvestartas@gmail.com>
2023-11-22 19:46:25 +0100

    LINT

--- src/compas_assembly2/data_sets/element/element_vault_and_columns_hex.py
+++ src/compas_assembly2/data_sets/element/element_vault_and_columns_hex.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     path = "src/compas_assembly2/data_sets/element/element_vault_and_columns_hex.json"
     elements_json = json_load(path)
 
-# ==========================================================================
-# VIEW2
-# ==========================================================================
-Viewer.show_elements(elements_json, show_grid=False, scale=0.001)
+    # ==========================================================================
+    # VIEW2
+    # ==========================================================================
+    Viewer.show_elements(elements_json, show_grid=False, scale=0.001)
--- src/compas_assembly2/data_sets/element/element_vault_plates_and_webs.py
+++ src/compas_assembly2/data_sets/element/element_vault_plates_and_webs.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     path = "src/compas_assembly2/data_sets/element/element_vault_plates_and_webs.json"
     elements_json = json_load(path)
 
-# ==========================================================================
-# VIEW2
-# ==========================================================================
-Viewer.show_elements(elements_json, show_grid=False, scale=0.001)
-#
+    # ==========================================================================
+    # VIEW2
+    # ==========================================================================
+    Viewer.show_elements(elements_json, show_grid=False, scale=0.001)
+    #
